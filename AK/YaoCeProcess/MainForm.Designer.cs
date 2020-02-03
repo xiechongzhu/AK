aedc64579993ac7aae1253a2822df9cee36bfd34
YangyGitHub <1543826974@qq.com>
2020-02-03 18:00:46 +0800

    创建遥测数据显示工程

---
+++ AK/YaoCeProcess/MainForm.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace YaoCeProcess
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.layoutControl_ToolBar = new DevExpress.XtraLayout.LayoutControl();
+            this.BtnStartStop = new DevExpress.XtraEditors.SimpleButton();
+            this.BtnSetting = new DevExpress.XtraEditors.SimpleButton();
+            this.Root = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem1 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem1 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControlItem2 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.emptySpaceItem2 = new DevExpress.XtraLayout.EmptySpaceItem();
+            this.layoutControl_MainContent = new DevExpress.XtraLayout.LayoutControl();
+            this.TabControl_SoftStatus = new DevExpress.XtraTab.XtraTabControl();
+            this.TabPage_XiTongPanJue = new DevExpress.XtraTab.XtraTabPage();
+            this.TabPage_DaoHangKuaiSu = new DevExpress.XtraTab.XtraTabPage();
+            this.layoutControlGroup1 = new DevExpress.XtraLayout.LayoutControlGroup();
+            this.layoutControlItem3 = new DevExpress.XtraLayout.LayoutControlItem();
+            this.TabPage_DaoHangManSu = new DevExpress.XtraTab.XtraTabPage();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl_ToolBar)).BeginInit();
+            this.layoutControl_ToolBar.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl_MainContent)).BeginInit();
+            this.layoutControl_MainContent.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.TabControl_SoftStatus)).BeginInit();
+            this.TabControl_SoftStatus.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // layoutControl_ToolBar
+            // 
+            this.layoutControl_ToolBar.AutoScroll = false;
+            this.layoutControl_ToolBar.Controls.Add(this.BtnStartStop);
+            this.layoutControl_ToolBar.Controls.Add(this.BtnSetting);
+            this.layoutControl_ToolBar.Dock = System.Windows.Forms.DockStyle.Top;
+            this.layoutControl_ToolBar.Location = new System.Drawing.Point(0, 0);
+            this.layoutControl_ToolBar.Name = "layoutControl_ToolBar";
+            this.layoutControl_ToolBar.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(1108, 0, 812, 500);
+            this.layoutControl_ToolBar.Root = this.Root;
+            this.layoutControl_ToolBar.Size = new System.Drawing.Size(1111, 62);
+            this.layoutControl_ToolBar.TabIndex = 0;
+            this.layoutControl_ToolBar.Text = "layoutControl1";
+            // 
+            // BtnStartStop
+            // 
+            this.BtnStartStop.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnStartStop.ImageOptions.Image")));
+            this.BtnStartStop.Location = new System.Drawing.Point(54, 12);
+            this.BtnStartStop.Name = "BtnStartStop";
+            this.BtnStartStop.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.BtnStartStop.Size = new System.Drawing.Size(38, 36);
+            this.BtnStartStop.StyleController = this.layoutControl_ToolBar;
+            this.BtnStartStop.TabIndex = 5;
+            // 
+            // BtnSetting
+            // 
+            this.BtnSetting.ImageOptions.Image = ((System.Drawing.Image)(resources.GetObject("BtnSet.ImageOptions.Image")));
+            this.BtnSetting.Location = new System.Drawing.Point(12, 12);
+            this.BtnSetting.Name = "BtnSetting";
+            this.BtnSetting.PaintStyle = DevExpress.XtraEditors.Controls.PaintStyles.Light;
+            this.BtnSetting.Size = new System.Drawing.Size(38, 36);
+            this.BtnSetting.StyleController = this.layoutControl_ToolBar;
+            this.BtnSetting.TabIndex = 4;
+            this.BtnSetting.Click += new System.EventHandler(this.BtnSetting_Click);
+            // 
+            // Root
+            // 
+            this.Root.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.Root.GroupBordersVisible = false;
+            this.Root.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem1,
+            this.emptySpaceItem1,
+            this.layoutControlItem2,
+            this.emptySpaceItem2});
+            this.Root.Name = "Root";
+            this.Root.Size = new System.Drawing.Size(1111, 62);
+            this.Root.TextVisible = false;
+            // 
+            // layoutControlItem1
+            // 
+            this.layoutControlItem1.Control = this.BtnSetting;
+            this.layoutControlItem1.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem1.Name = "layoutControlItem1";
+            this.layoutControlItem1.Size = new System.Drawing.Size(42, 40);
+            this.layoutControlItem1.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem1.TextVisible = false;
+            // 
+            // emptySpaceItem1
+            // 
+            this.emptySpaceItem1.AllowHotTrack = false;
+            this.emptySpaceItem1.Location = new System.Drawing.Point(0, 40);
+            this.emptySpaceItem1.MaxSize = new System.Drawing.Size(84, 10);
+            this.emptySpaceItem1.MinSize = new System.Drawing.Size(84, 1);
+            this.emptySpaceItem1.Name = "emptySpaceItem1";
+            this.emptySpaceItem1.Size = new System.Drawing.Size(84, 2);
+            this.emptySpaceItem1.SizeConstraintsType = DevExpress.XtraLayout.SizeConstraintsType.Custom;
+            this.emptySpaceItem1.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControlItem2
+            // 
+            this.layoutControlItem2.Control = this.BtnStartStop;
+            this.layoutControlItem2.Location = new System.Drawing.Point(42, 0);
+            this.layoutControlItem2.Name = "layoutControlItem2";
+            this.layoutControlItem2.Size = new System.Drawing.Size(42, 40);
+            this.layoutControlItem2.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem2.TextVisible = false;
+            // 
+            // emptySpaceItem2
+            // 
+            this.emptySpaceItem2.AllowHotTrack = false;
+            this.emptySpaceItem2.Location = new System.Drawing.Point(84, 0);
+            this.emptySpaceItem2.Name = "emptySpaceItem2";
+            this.emptySpaceItem2.Size = new System.Drawing.Size(1007, 42);
+            this.emptySpaceItem2.TextSize = new System.Drawing.Size(0, 0);
+            // 
+            // layoutControl_MainContent
+            // 
+            this.layoutControl_MainContent.Controls.Add(this.TabControl_SoftStatus);
+            this.layoutControl_MainContent.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.layoutControl_MainContent.Location = new System.Drawing.Point(0, 62);
+            this.layoutControl_MainContent.Name = "layoutControl_MainContent";
+            this.layoutControl_MainContent.OptionsCustomizationForm.DesignTimeCustomizationFormPositionAndSize = new System.Drawing.Rectangle(687, 146, 812, 500);
+            this.layoutControl_MainContent.Root = this.layoutControlGroup1;
+            this.layoutControl_MainContent.Size = new System.Drawing.Size(1111, 651);
+            this.layoutControl_MainContent.TabIndex = 1;
+            this.layoutControl_MainContent.Text = "layoutControl2";
+            // 
+            // TabControl_SoftStatus
+            // 
+            this.TabControl_SoftStatus.Location = new System.Drawing.Point(12, 12);
+            this.TabControl_SoftStatus.Name = "TabControl_SoftStatus";
+            this.TabControl_SoftStatus.SelectedTabPage = this.TabPage_XiTongPanJue;
+            this.TabControl_SoftStatus.Size = new System.Drawing.Size(1087, 627);
+            this.TabControl_SoftStatus.TabIndex = 4;
+            this.TabControl_SoftStatus.TabPages.AddRange(new DevExpress.XtraTab.XtraTabPage[] {
+            this.TabPage_XiTongPanJue,
+            this.TabPage_DaoHangKuaiSu,
+            this.TabPage_DaoHangManSu});
+            // 
+            // TabPage_XiTongPanJue
+            // 
+            this.TabPage_XiTongPanJue.Name = "TabPage_XiTongPanJue";
+            this.TabPage_XiTongPanJue.Size = new System.Drawing.Size(1085, 595);
+            this.TabPage_XiTongPanJue.Text = "系统判决状态";
+            // 
+            // TabPage_DaoHangKuaiSu
+            // 
+            this.TabPage_DaoHangKuaiSu.Name = "TabPage_DaoHangKuaiSu";
+            this.TabPage_DaoHangKuaiSu.Size = new System.Drawing.Size(1085, 595);
+            this.TabPage_DaoHangKuaiSu.Text = "导航数据（快速）";
+            // 
+            // layoutControlGroup1
+            // 
+            this.layoutControlGroup1.EnableIndentsWithoutBorders = DevExpress.Utils.DefaultBoolean.True;
+            this.layoutControlGroup1.GroupBordersVisible = false;
+            this.layoutControlGroup1.Items.AddRange(new DevExpress.XtraLayout.BaseLayoutItem[] {
+            this.layoutControlItem3});
+            this.layoutControlGroup1.Name = "Root";
+            this.layoutControlGroup1.Size = new System.Drawing.Size(1111, 651);
+            this.layoutControlGroup1.TextVisible = false;
+            // 
+            // layoutControlItem3
+            // 
+            this.layoutControlItem3.Control = this.TabControl_SoftStatus;
+            this.layoutControlItem3.Location = new System.Drawing.Point(0, 0);
+            this.layoutControlItem3.Name = "layoutControlItem3";
+            this.layoutControlItem3.Size = new System.Drawing.Size(1091, 631);
+            this.layoutControlItem3.TextSize = new System.Drawing.Size(0, 0);
+            this.layoutControlItem3.TextVisible = false;
+            // 
+            // TabPage_DaoHangManSu
+            // 
+            this.TabPage_DaoHangManSu.Name = "TabPage_DaoHangManSu";
+            this.TabPage_DaoHangManSu.Size = new System.Drawing.Size(1085, 595);
+            this.TabPage_DaoHangManSu.Text = "导航数据（慢速）";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1111, 713);
+            this.Controls.Add(this.layoutControl_MainContent);
+            this.Controls.Add(this.layoutControl_ToolBar);
+            this.Name = "MainForm";
+            this.Text = "遥测数据显示";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl_ToolBar)).EndInit();
+            this.layoutControl_ToolBar.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.Root)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.emptySpaceItem2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControl_MainContent)).EndInit();
+            this.layoutControl_MainContent.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.TabControl_SoftStatus)).EndInit();
+            this.TabControl_SoftStatus.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlGroup1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.layoutControlItem3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevExpress.XtraLayout.LayoutControl layoutControl_ToolBar;
+        private DevExpress.XtraEditors.SimpleButton BtnStartStop;
+        private DevExpress.XtraEditors.SimpleButton BtnSetting;
+        private DevExpress.XtraLayout.LayoutControlGroup Root;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem1;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem2;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem2;
+        private DevExpress.XtraLayout.LayoutControl layoutControl_MainContent;
+        private DevExpress.XtraLayout.LayoutControlGroup layoutControlGroup1;
+        private DevExpress.XtraTab.XtraTabControl TabControl_SoftStatus;
+        private DevExpress.XtraTab.XtraTabPage TabPage_XiTongPanJue;
+        private DevExpress.XtraTab.XtraTabPage TabPage_DaoHangKuaiSu;
+        private DevExpress.XtraLayout.LayoutControlItem layoutControlItem3;
+        private DevExpress.XtraLayout.EmptySpaceItem emptySpaceItem1;
+        private DevExpress.XtraTab.XtraTabPage TabPage_DaoHangManSu;
+    }
+}
+
---
+++ AK/YaoCeProcess/Protocol.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YaoCeProcess
+{
+    // UDP包头
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct UDPHead
+    { 
+        public byte         header1;            // 固定0xAA
+        public byte         header2;            // 固定0x00
+        public byte         header3;            // 固定0x55
+        public byte         header4;            // 固定0x77
+
+        public UInt16       dataLength;         // 数据长度L
+        // 后面直接跟<=645个字节数据
+    }
+
+    //----------------------------------------------------------------------------//
+    // CAN数据帧协议头
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct CANHead 
+    {
+        public byte         frameInfo;          // 包含了该帧内的数据长度(bit0-bit3)
+        // 后面直接跟数据段第1-8个字节
+    }
+
+    // 第一子帧帧结构
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct CANFirst
+    { 
+        public byte         xuHao;              // 子帧序号 第一帧固定为0x00
+        public byte         zhenZongChang;      // 帧总长度
+        public UInt16       zhenBianHao;        // 帧编号（0-65535循环计数）??使用待定
+        public byte         shuJuDuanZongChangDu;   // 数据段总长度
+        public byte         zhenLeiXing;        // 帧类型
+    }
+
+    // 中间帧帧结构
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct CANCenter
+    {
+        public byte         xuHao;              // 子帧序号 依次递增
+        // 后面直接跟数据段7个字节
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 7)]
+        public byte[]       data;
+    }
+
+    // 结束帧帧结构
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct CANEnd
+    {
+        public byte         xuHao;              // 子帧序号 依次递增
+        // 后面直接跟数据段1-5个字节数据
+        // 跟两个字节的CRC16校验结果
+    }
+
+    //----------------------------------------------------------------------------//
+
+    // 系统判据状态数据
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct SYSTEMPARSE_STATUS
+    {
+        public double       jingDu;             // 经度
+        public double       weiDu;              // 纬度
+        public float        haiBaGaoDu;         // 海拔高度
+
+        public float        dongXiangSuDu;      // 东向速度
+        public float        beiXiangSuDu;       // 北向速度
+        public float        tianXiangSuDu;      // 天向速度
+
+        public float        WxJiaoSuDu;         // Wx角速度
+        public float        WyJiaoSuDu;         // Wy角速度
+        public float        WzJiaoSuDu;         // Wz角速度
+
+        public float        zhouXiangGuoZai;    // 轴向过载
+        public float        GNSSTime;           // GNSS时间
+
+        public float        curFaSheXi_X;       // 当前发射系X
+        public float        curFaSheXi_Y;       // 当前发射系Y
+        public float        curFaSheXi_Z;       // 当前发射系Z
+
+        public float        yuShiLuoDianSheCheng;   // 预示落点射程
+        public float        yuShiLuoDianZ;      // 预示落点Z
+        public double       feiXingZongShiJian; // 飞行总时间
+
+        public byte         ceLueJieDuan;       // 策略阶段(0-准备 1-起飞 2-一级 3-二级 4-结束)
+        public byte         danTouZhuangTai;    // 弹头状态(0-状态异常 1-产品遥测上电正常 2-初始化正常 3-一级保险解除
+                                                // 4-二级保险解除 5-收到保险解除信号 6-三级保险解除 7-充电 8-起爆)
+
+        public byte         daoHangTip1;        // 导航状态提示1
+                                                // bit0 导航数据选择结果（0：数据不可用 1：数据可用）
+                                                // bit1 陀螺数据融合结果（0：所有数据不可用 1：数据可用）
+                                                // bit2 bit3 数据未更新标志（00：均无数据; 01：1号输入无数据，2号输入有数据;
+                                                //                           10：1号输入有数据，2号输入无数据; 11：均有数据）
+                                                // bit4 bit5 时间间隔异常标志（00：时间间隔均正常; 01：1号时间间隔异常，2号时间间隔正常； 10：1号时间间隔正常，2号时间间隔异常； 00：时间间隔均不正常）
+                                                // bit6 弹头组合无效标志（1表示无效）
+                                                // bit7 弹体组合无效标志（1表示无效）
+
+        public byte         daoHangTip2;        // 导航状态提示2
+                                                // bit0 bit1 1号数据经度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit2 bit3 1号数据纬度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit4 bit5 1号数据高度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit6 bit7 1号数据东向速度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+
+        public byte         daoHangTip3;        // 导航状态提示3
+                                                // bit0 bit1 1号数据北向速度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit2 bit3 1号数据天向速度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit4 bit5 2号数据经度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit6 bit7 2号数据纬度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+
+        public byte         daoHangTip4;        // 导航状态提示4
+                                                // bit0 bit1 2号数据高度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit2 bit3 2号数据东向速度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit4 bit5 2号数据北向速度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+                                                // bit6 bit7 2号数据天向速度标志（00：不是野值；01：无数据；10：数据用于初始化；11：是野值）
+
+        public byte         sysyemStatusTip;    // 系统状态指示
+                                                // bit0 功率输出闭合（1有效）
+                                                // bit1 解保指令发出（1有效）
+                                                // bit2 自毁指令发出（1有效）
+                                                // bit3 复位信号（1有效）
+                                                // bit4 对外供电（1有效）
+                                                // bit5 模拟自毁指令1（1有效）
+                                                // bit6 模拟自毁指令2（1有效）
+                                                // bit7 回路检测 ?? 待定
+
+        public byte         chuDianZhuangTai;   // 触点状态指示
+                                                // bit0 起飞分离脱插信号（0有效）
+                                                // bit1 一级分离脱插信号（0有效）
+                                                // bit2 安控接收机预令（1有效）
+                                                // bit3 安控接收机动令（1有效）
+                                                // bit4 一级自毁工作状态A（1有效）
+                                                // bit5 一级自毁工作状态B（1有效）
+                                                // bit6 二级自毁工作状态A（1有效）
+                                                // bit7 二级自毁工作状态B（1有效）
+
+        public byte         jueCePanJueJieGuo1; // 策略判决结果1
+                                                // bit0 总飞行时间（1：有效）
+                                                // bit1 侧向（1：有效）
+                                                // bit2 Wx角速度（1：有效）
+                                                // bit3 Wy角速度（1：有效）
+                                                // bit4 Wz角速度（1：有效）
+                                                // bit5 后向（1：有效）
+                                                // bit6 坠落（1：有效）
+                                                // bit7 分离时间（1：有效）
+
+        public byte         jueCePanJueJieGuo2; // 策略判决结果2
+                                                // bit0 控制区下限（1：有效）
+                                                // bit1 控制区上限（1：有效）
+
+        public byte         shuChuKaiGuanStatus1; // 输出开关状态1
+                                                  // bit0 弹头保险（1：闭合）
+                                                  // bit1 弹头起爆（1：闭合）
+                                                  // bit2 一级保险1（1：闭合）
+                                                  // bit3 一级保险2（1：闭合）
+                                                  // bit4 一级起爆1（1：闭合）
+                                                  // bit5 一级起爆2（1：闭合）
+                                                  // bit6 二级保险1（1：闭合）
+                                                  // bit7 二级保险2（1：闭合）
+
+        public byte         shuChuKaiGuanStatus2; // 输出开关状态2
+                                                  // bit0 二级起爆1（1：闭合）
+                                                  // bit1 二级起爆2（1：闭合）
+                                                  // bit2 bit3 参试状态（00：测试1，数据输出状态；01：测试2，低压输出状态；10：保留状态；11：正式实验）
+    }
+
+    // 导航数据(快速)
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct DAOHANGSHUJU_KuaiSu
+    {
+
+    }
+
+    // 导航数据(慢速)
+    [Serializable]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    struct DAOHANGSHUJU_ManSu
+    {
+
+    }
+}
