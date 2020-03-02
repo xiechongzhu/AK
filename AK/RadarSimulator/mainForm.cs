fea44debe491d0c22b62c119ffbbb24c6c705258
YangyGitHub <1543826974@qq.com>
2020-03-02 16:34:46 +0800

    add RadarSimulator excel read

--- AK/RadarSimulator/mainForm.cs
+++ AK/RadarSimulator/mainForm.cs
@@ -1,7 +1,11 @@
-﻿using System;
+﻿using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using NPOI.XSSF.UserModel;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -17,6 +21,18 @@ namespace RadarSimulator
     {
         private UdpClient udpClient = null;
         private bool isLoopSend = false;
+
+        // 默认打开路径
+        private string InitialDirectory = "D:\\";
+
+        // 创建文件对象
+        FileStream fileStream = null;
+        // 创建工作簿对象
+        XSSFWorkbook workbook = null;
+        // 读取工作簿第一张表(此处参数可为下标,也可为表名)
+        ISheet sheet = null;
+        // 读取当前行数(从1行开始读取，第0行为标题)
+        int curReadLine = 29;
 
         public mainForm()
         {
@@ -34,6 +50,34 @@ namespace RadarSimulator
         {
             if (!isLoopSend)
             {
+                //-------------------------------------------//
+                // 选择文件
+                OpenFileDialog openFileDialog = new OpenFileDialog();//打开文件对话框              
+                if (InitialDialog(openFileDialog, "Open"))
+                {
+                    try
+                    {
+                        using (Stream stream = openFileDialog.OpenFile())
+                        {
+                            string FileName = ((System.IO.FileStream)stream).Name;
+                            // 执行相关文件操作
+                            //---------------------------------------------------//
+                            //创建文件对象
+                            fileStream = new FileStream(FileName, FileMode.Open, FileAccess.Read);
+                            // 创建工作簿对象
+                            workbook = new XSSFWorkbook(fileStream);
+                            // 读取工作簿第一张表(此处参数可为下标,也可为表名)
+                            sheet = workbook.GetSheetAt(1);
+                        }
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        return;
+                    }
+                }
+
+                //-------------------------------------------//
                 timer.Interval = int.Parse(editInterval.Text);
                 timer.Start();
                 btnSendLoop.Text = "停止";
@@ -58,7 +102,7 @@ namespace RadarSimulator
                     udpClient = new UdpClient();
                     btnOpen.Text = "关闭";
                 }
-                catch(Exception ex)
+                catch (Exception ex)
                 {
                     MessageBox.Show("打开端口失败:" + ex.Message, "错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
@@ -115,10 +159,73 @@ namespace RadarSimulator
 
         private void timer_Tick(object sender, EventArgs e)
         {
+            /*
             Random random = new Random();
             byte[] sendBuffer = BuildPacket(random.NextDouble() * 1000, random.NextDouble() * 1000, random.NextDouble() * 1000,
                 random.NextDouble() * 1000, random.NextDouble() * 1000, random.NextDouble() * 1000);
             udpClient?.Send(sendBuffer, sendBuffer.Length, editIp.Text, int.Parse(editPort.Text));
+            */
+
+            if (sheet == null) return;
+            // 新建当前工作表行数据
+            IRow row;
+            if (curReadLine <= sheet.LastRowNum)
+            {
+                // row读入第i行数据
+                row = sheet.GetRow(curReadLine++);
+                // 获取每一列的数据,并转换为对应的数据类型.
+                double x = System.Convert.ToDouble(row.GetCell(2).ToString());
+                double y = System.Convert.ToDouble(row.GetCell(3).ToString());
+                double z = System.Convert.ToDouble(row.GetCell(4).ToString());
+
+                double Vx = System.Convert.ToDouble(row.GetCell(5).ToString());
+                double Vy = System.Convert.ToDouble(row.GetCell(6).ToString());
+                double Vz = System.Convert.ToDouble(row.GetCell(7).ToString());
+
+                // 发送数据
+                byte[] sendBuffer = BuildPacket(x * 1000, y * 1000, z * 1000,
+                Vx * 1000, Vy * 1000, Vz * 1000);
+                udpClient?.Send(sendBuffer, sendBuffer.Length, editIp.Text, int.Parse(editPort.Text));
+            }
+            else 
+            {
+                // 释放资源
+                fileStream = null;
+                workbook = null;
+                sheet = null;
+
+                timer.Stop();
+                btnSendLoop.Text = "循环发送";
+                btnSendOne.Enabled = true;
+                isLoopSend = false;
+            }
+        }
+
+        //统一对话框
+        private bool InitialDialog(FileDialog fileDialog, string title)
+        {
+            fileDialog.InitialDirectory = InitialDirectory;                  // 初始化路径
+            fileDialog.Filter = "excel files (*.xls,*.xlsx)|*.xls;*.xlsx";   // 过滤选项设置，文本文件，所有文件。
+            fileDialog.FilterIndex = 1;                                      // 当前使用第二个过滤字符串
+            fileDialog.RestoreDirectory = true;                              // 对话框关闭时恢复原目录
+            fileDialog.Title = title;
+            if (fileDialog.ShowDialog() == DialogResult.OK)
+            {
+                for (int i = 1; i <= fileDialog.FileName.Length; i++)
+                {
+                    if (fileDialog.FileName.Substring(fileDialog.FileName.Length - i, 1).Equals(@"\"))
+                    {
+                        // 更改默认路径为最近打开路径
+                        InitialDirectory = fileDialog.FileName.Substring(0, fileDialog.FileName.Length - i + 1);
+                        return true;
+                    }
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
